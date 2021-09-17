0f05adc5347ebfa341b43f43ee97a1e9fb8ca916
menddang <menddang2357@gmail.com>
2021-09-18 00:01:57 +0900

    dataset return audio for discriminator

--- datasets/ljspeech.py
+++ datasets/ljspeech.py
@@ -44,20 +44,33 @@ class LJSpeechDataset(AudioDataset):
         )
         
 
-    def Write(self, path):
+    def Write(self, train_tfrecord_path, valid_tfrecord_path):
         # Serialize dataset to save as tfrecord format
-        self.dataset = self.dataset.map(
-            self._serializeExample,
+        self.train_dataset = self.train_dataset.map(
+            self._SerializeExample,
             num_parallel_calls=tf.data.experimental.AUTOTUNE
         )
 
-        writer = tf.data.experimental.TFRecordWriter(path)
-        writer.write(self.dataset)
+        writer = tf.data.experimental.TFRecordWriter(train_tfrecord_path)
+        writer.write(self.train_dataset)
 
-    def Load(self, path):
-        self.dataset = tf.data.TFRecordDataset(path)
-        self.dataset = self.dataset.map(
-            self._parseExample,
+        self.valid_dataset = self.valid_dataset.map(
+            self._SerializeExample,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+
+        writer = tf.data.experimental.TFRecordWriter(valid_tfrecord_path)
+        writer.write(self.valid_dataset)
+
+    def Load(self, train_tfrecord_path, valid_tfrecord_path):
+        self.train_dataset = tf.data.TFRecordDataset(train_tfrecord_path)
+        self.train_dataset = self.train_dataset.map(
+            self._ParseExample,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+        self.valid_dataset = tf.data.TFRecordDataset(valid_tfrecord_path)
+        self.valid_dataset = self.valid_dataset.map(
+            self._ParseExample,
             num_parallel_calls=tf.data.experimental.AUTOTUNE
         )
 
@@ -102,22 +115,26 @@ class LJSpeechDataset(AudioDataset):
         text = 'S' + text + 'E'
         tokens = self.tokenizer(tf.reshape(text, ()))
 
-        return mel, tf.shape(mel)[0], tokens
+        return audio, tf.shape(audio)[0], mel, tf.shape(mel)[0], tokens, tf.shape(tokens)[0]
 
-    def _SerializeExample(self, mel, mel_length, tokens, token_length):
+    def _SerializeExample(self, audio, audio_length, mel, mel_length, tokens, token_length):
 
         def _bytes_features(value):
             if isinstance(value, type(tf.constant(0))):
                 value = value.numpy()
             return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-        def _serialize(_mel, _mel_length, _tokens, _token_length):
+        def _serialize(_audio, _audio_length, _mel, _mel_length, _tokens, _token_length):
+            serialized_audio = tf.io.serialize_tensor(_audio)
+            serialized_audio_length = tf.io.serialize_tensor(_audio_length)
             serialized_mel = tf.io.serialize_tensor(_mel)
             serialized_mel_length = tf.io.serialize_tensor(_mel_length)
             serialized_tokens = tf.io.serialize_tensor(_tokens)
             serialized_token_length = tf.io.serialize_tensor(_token_length)
 
             feature = {
+                'audio': _bytes_features(serialized_audio),
+                'audio_length': _bytes_features(serialized_audio_length),
                 'mel': _bytes_features(serialized_mel),
                 'mel_length': _bytes_features(serialized_mel_length),
                 'tokens': _bytes_features(serialized_tokens),
@@ -130,7 +147,7 @@ class LJSpeechDataset(AudioDataset):
 
         output = tf.py_function(
             _serialize,
-            inp=[mel, mel_length, tokens, token_length],
+            inp=[audio, audio_length, mel, mel_length, tokens, token_length],
             Tout=[tf.string])
 
         return tf.reshape(output, ())
@@ -138,6 +155,8 @@ class LJSpeechDataset(AudioDataset):
     def _ParseExample(self, serialized_example):
 
         parse_dict = {
+            'audio': tf.io.FixedLenFeature([], tf.string),
+            'audio_length': tf.io.FixedLenFeature([], tf.string),
             'mel': tf.io.FixedLenFeature([], tf.string),
             'mel_length': tf.io.FixedLenFeature([], tf.string),
             'tokens': tf.io.FixedLenFeature([], tf.string),
@@ -146,11 +165,13 @@ class LJSpeechDataset(AudioDataset):
 
         example = tf.io.parse_single_example(serialized_example, parse_dict)
 
+        audio = tf.io.parse_tensor(example['audio'], out_type=tf.float32)
+        audio_length = tf.io.parse_tensor(example['audio_length'], out_type=tf.int32)
         mel = tf.io.parse_tensor(example['mel'], out_type=tf.float32)
         mel_length = tf.io.parse_tensor(example['mel_length'], out_type=tf.int32)
         tokens = tf.io.parse_tensor(example['tokens'], out_type=tf.int32)
         token_length = tf.io.parse_tensor(example['token_length'], out_type=tf.int32)
-        return (mel, mel_length, tokens, token_length)
+        return (audio, audio_length, mel, mel_length, tokens, token_length)
 
     @staticmethod
     def ParseArgument(parser: argparse.ArgumentParser):
